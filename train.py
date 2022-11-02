ca376939ca2312117ee9411b4776da537d1f2f08
Eduardo Nevess <edu.seven2000@gmail.com>
2022-11-02 20:55:02 +0000

    kfkfkfkf

--- train.py
+++ train.py
@@ -5,37 +5,29 @@ import math
 import pickle
 import os
 import nltk
+import json
 from model import seq2seq_model,pad_sentence,get_accuracy,sentence_to_seq
+
+from nltk.stem import WordNetLemmatizer
 #import config
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 #tf.logging.set_verbosity(tf.logging.ERROR)
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-
-
-import random
-import json
-import pickle
-import numpy as np
-import nltk
-  
-from keras.models import Sequential
-from nltk.stem import WordNetLemmatizer
-from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import SGD
   
   
 lemmatizer = WordNetLemmatizer()
   
 # reading the json.intense file
-intents = json.loads(open("intense.json").read())
+intents = json.loads(open("test_data.json").read())
+print(type(intents))
   
 # creating empty lists to store data
 words = []
 classes = []
 documents = []
 ignore_letters = ["?", "!", ".", ","]
-for intent in intents['intents']:
+for key, values in intents.items():
     for pattern in intent['patterns']:
         # separating words from patterns
         word_list = nltk.word_tokenize(pattern)
@@ -57,4 +49,28 @@ words = sorted(set(words))
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
+
+# we need numerical values of the
+# words because a neural network
+# needs numerical values to work with
+training = []
+output_empty = [0]*len(classes)
+for document in documents:
+	bag = []
+	word_patterns = document[0]
+	word_patterns = [lemmatizer.lemmatize(
+		word.lower()) for word in word_patterns]
+	for word in words:
+		bag.append(1) if word in word_patterns else bag.append(0)
+		
+	# making a copy of the output_empty
+	output_row = list(output_empty)
+	output_row[classes.index(document[1])] = 1
+	training.append([bag, output_row])
+random.shuffle(training)
+training = np.array(training)
+
+# splitting the data
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
 
