19731bf5cb697734ebddb94a7a2e239f04b62107
eduardo <edu.seven2000@gmail.com>
2022-11-07 14:52:46 +0000

    mais

---
+++ to_json.py
@@ -0,0 +1,25 @@
+from xml.etree import cElementTree as ET
+import json
+
+
+tree = ET.parse('4625179.xml')
+root = tree.getroot()
+
+#root = ET.fromstring(tree)
+txt = ""
+iter = 1
+dict = {}
+for word in root.iter('w'):
+    
+    if word.text.find(',') != -1:
+        txt = txt[:-1]
+    elif word.text.find('.') != -1: 
+        dict.update({'line {}'.format(iter): txt})
+        iter += 1
+        txt = ""
+        continue
+    txt += word.text + " "  
+
+
+with open("test_data.json", "w", encoding='utf8') as file:
+    my_dict = json.dump(dict, file, ensure_ascii=False)
